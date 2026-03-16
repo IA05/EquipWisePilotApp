2ddb1bf9d5ccd49a9cb59656810a92868c2c15cf
faith7867 <ahmedimaan24025@gmail.com>
2026-03-16 12:32:16 +0000

    Completed ItemList.java & fixed ItemOperations.java!

--- EquipWise/src/ADTs/ItemList.java
+++ EquipWise/src/ADTs/ItemList.java
@@ -4,10 +4,115 @@
  */
 package ADTs;
 
+//IMPORTS
+import equipwise.Item;
+import equipwise.ItemOperations;
+
 /**
  *
  * @author ImaanAhmed
  */
 public class ItemList implements ItemOperations {
     
-}
+    //
+    private ItemNode head;
+    private int size;
+    
+    //
+    public ItemList() {
+        head = null;
+        size = 0;
+    }
+    
+    //METHOD TO INSERT (at end)
+    @Override
+    public void insert(Item item) {
+        
+        ItemNode newNode = new ItemNode(item);  //
+        //
+        if(head == null) {
+            head = newNode;
+        } else {
+            ItemNode current = head;
+            //
+            while(current.getNext() != null) {
+                current = current.getNext();
+            } //end while loop
+            current.setNext(newNode);
+        } //end if/else statement
+        
+        size++;
+        
+    } //end insert()
+    
+    
+    //METHOD TO DELETE (by ID instead of index... simplifying user interaction with GUI)
+    @Override
+    public void delete(int id) {
+        
+        if(head == null) return;    //
+        //
+        if(head.getData().getId() == id) {
+            head = head.getNext();
+            size--;
+            return;
+        } //end if statement
+        
+        //
+        ItemNode current = head;    //
+        ItemNode prev = null;   //
+        
+        //
+        while(current != null && current.getData().getId() != id) {
+            prev = current;
+            current = current.getNext();
+        } //end while loop
+        
+        //
+        if(current != null) {
+            prev.setNext(current.getNext());
+            size--;
+        } //end if statement
+        
+    } //end delete()
+    
+    
+    //MEHTOD TO SEARCH (by name)
+    @Override
+    public Item search(String name) {
+        
+        ItemNode current = head;    //
+        
+        //
+        while(current != null) {
+            if(current.getData().getName().equalsIgnoreCase(name)) {
+                return current.getData();   //
+            } //end if statement
+        } //end while loop
+        
+        return null;
+        
+    } //end search()
+    
+    
+    //DISPLAY METHOD
+    @Override
+    public void display() {
+        
+        ItemNode current = head;    //
+        
+        //Looping through...
+        while(current != null) {
+            System.out.println(current.getData().displayInfo());
+        } //end while loop
+        
+    } //end display()
+    
+    
+    //METHOD TO CHECK LIST SIZE
+    public int getSize() {
+        return size;
+    } //end getSize()
+    
+    
+} //end class
