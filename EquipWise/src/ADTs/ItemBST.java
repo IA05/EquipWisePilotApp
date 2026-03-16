c340c9c44e2ad6f317bd16f9ba2b9357d533eccc
faith7867 <ahmedimaan24025@gmail.com>
2026-03-16 13:01:36 +0000

    Progress on ADT2 (BTree) - ItemBST.java

--- EquipWise/src/ADTs/ItemBST.java
+++ EquipWise/src/ADTs/ItemBST.java
@@ -5,14 +5,70 @@
 package ADTs;
 
 //IMPORTS
-import equipwise.BrowseUI;
+import equipwise.Item;
+import equipwise.ItemOperations;
 
 /**
  *
  * @author ImaanAhmed
  */
-public class ItemBST implements BrowseUI{
+public class ItemBST implements ItemOperations{
     
-    //
+    private TreeNode root;    //
+
+    //COMSTRUCTOR
+    public ItemBST() {
+        root = null;
+    }
     
-}
+    
+    //RETURNING INSERT METHOD
+    @Override
+    public void insert(Item item) {
+        root = insertRec(root, item);
+    }
+    
+    
+    //METHOD TO INSERT
+    private TreeNode insertRec(TreeNode current, Item item) {
+        //
+        if(current == null) {
+            return new TreeNode(item);
+        }
+        
+        //Comparing by ID
+        if(item.getId() < current.getData().getId()) {
+            current.setLeft(insertRec(current.getLeft(), item));
+        } else {
+            current.setRight(insertRec(current.getRight(), item));
+        } //end if/else statement
+        
+        return current;     //
+    } //end insertRec
+    
+    
+    //RETURNING SEARCH METHOD
+    @Override
+    public Item search(String name) {
+        return searchRec(root, name);
+    } //end search()
+    
+    
+    //METHOD TO SEARCH (by name)
+    private Item searchRec(TreeNode current, String name) {
+        
+        if(current == null) return null;    //
+        //
+        if(current.getData().getName().equalsIgnoreCase(name)) {
+            return current.getData();
+        }
+        //Sesarching on both sides since BST is ordered by ID, not name
+        Item found = searchRec(current.getLeft(), name);
+        if(found != null) return found;
+        
+        return searchRec(current.getRight(), name);
+        
+    } //end searchRec
+    
+    
+} //end class
